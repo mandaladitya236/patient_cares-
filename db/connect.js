0c7ccfd836afa4657263c1637d16b6ab31e3f633
mandaladitya236 <mandaladitya236@gmail.com>
2022-03-07 09:34:45 +0530

    changes

--- db/connect.js
+++
@@ -1,8 +0,0 @@
-/* Mongo Database
-* - this is where we set up our connection to the mongo database
-*/
-const mongoose = require('mongoose')
-const connectDB=() =>{
-	return mongoose.connect(url)
-}
- export default connectDB
